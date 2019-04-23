f6d7b071ce6f5e93e6316196ade04760d9728782
N Nemakhavhani <ndamulelo@bbd.co.za>
2019-04-23 21:06:21 +0200

    Update recipes api controller

--- src/main/java/com/foodies/foodies/restApi/RecipesController.java
+++ src/main/java/com/foodies/foodies/restApi/RecipesController.java
@@ -1,8 +1,6 @@
 package com.foodies.foodies.restApi;
 
-import com.foodies.foodies.DAO.UserProfilesRepository;
 import com.foodies.foodies.Models.Recipe;
-import com.foodies.foodies.Models.UserProfile;
 import com.foodies.foodies.Services.IRecipesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,15 +14,13 @@ import java.util.List;
 @RequestMapping("api")
 public class RecipesController {
     private IRecipesService _recipeService;
-    @Autowired
-    private UserProfilesRepository _users;
 
     @Autowired
     public RecipesController(IRecipesService recipeService) {
         this._recipeService = recipeService;
     }
 
-    @GetMapping("/all")
+    @GetMapping("/recipes")
     public ResponseEntity<List<Recipe>> RetriveAllRecipes(){
 
         List<Recipe> results = new ArrayList<>();
@@ -35,42 +31,57 @@ public class RecipesController {
     }
 
 
-    @GetMapping("all/{id}")
-    public ResponseEntity<Recipe> RetriveRecipeByID(@PathVariable("id") Long ID){
+    @GetMapping("recipes/{id}")
+    public ResponseEntity RetriveRecipeByID(@PathVariable("id") Long ID){
 
         Recipe result = _recipeService.FindRecipeByID(ID).orElse(null);
 
         return result != null?  new ResponseEntity(result, HttpStatus.OK) : new ResponseEntity(HttpStatus.NOT_FOUND);
     }
 
+    @PostMapping("recipes/create")
+    public ResponseEntity<Recipe> CreateRecipe(@RequestBody Recipe recipe){
 
+        boolean result = _recipeService.CreateRecipe(recipe);
 
-
-
-
-
-
-//    Todo: Remove after testing!!
-    @GetMapping("/users")
-    public ResponseEntity<List<UserProfile>> RetriveAllUsers(){
-
-        List<UserProfile> results = new ArrayList<>();
-
-        _users.findAll().forEach(item -> results.add((item)));
-
-        return new ResponseEntity(results, HttpStatus.OK);
+        return this.GenerateNoContentResponse(result);
     }
 
-    @PostMapping("user/create")
-    public  String SaveUserProfile(@RequestBody UserProfile usr){
-        try{
-            _users.save(usr);
-            return "Result: " + HttpStatus.OK;
-        }catch (Exception e){
-            return "Error: " + e;
+    @PutMapping("recipes/{id}")
+    public ResponseEntity<Recipe> UpdateRecipe(@PathVariable("id") Long ID, @RequestBody Recipe recipe){
+
+        boolean result = _recipeService.UpdateRecipe(ID, recipe);
+
+        return this.GenerateNoContentResponse(result);
+    }
+
+    @DeleteMapping("recipes/{id}")
+    public ResponseEntity<Recipe> DeleteRecipe(@PathVariable("id") Long ID){
+
+        boolean result = _recipeService.DeleteRecipe(0L, ID);
+
+        return this.GenerateNoContentResponse(result);
+    }
+
+
+
+//Internal helpers
+    private ResponseEntity GenerateNoContentResponse(boolean result){
+        if(result == true){
+
+            Object responseBody = new Object(){
+                public final boolean succeeded = result;
+                public final String message ="Operation completed successfully.";
+            };
+            return new ResponseEntity(responseBody, HttpStatus.NO_CONTENT);
+
+        }else{
+            Object responseBody = new Object(){
+                public final boolean succeeded = result;
+                public final String message ="Error: Please correct your schema and try again.";
+            };
+            return new ResponseEntity(responseBody, HttpStatus.BAD_REQUEST);
         }
-
-
 
     }
 
