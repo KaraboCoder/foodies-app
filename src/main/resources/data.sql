c509658b82b9bebe02be106e4a23ccfb67e0b054
N Nemakhavhani <ndamulelo@bbd.co.za>
2019-04-23 13:25:45 +0200

    update seed data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,22 @@
 /* DB Seed data */
 
 --Recipes
-insert into userprofiles values (1, 'endee', 'endee@outlook.com')
+insert into recipes values (1, 'Tikka', 1, 'No description',
+    '1. Start with water', 'no-picture', '2 mins', 'EASY')
 ON DUPLICATE KEY UPDATE ID=ID;
 
 --Ingredients
-insert into userprofiles values (1, 'endee', 'endee@outlook.com')
+insert into ingredients values (1, 'Chicken nugets', 'Grams',
+23, 15, 'be careful', 1)
 ON DUPLICATE KEY UPDATE ID=ID;
 
 --Categories
-insert into userprofiles values (1, 'endee', 'endee@outlook.com')
+insert into recipeCategories values (1, 'Chinese', 'made in china')
 ON DUPLICATE KEY UPDATE ID=ID;
 
 --User profiles
 insert into userprofiles values (1, 'endee', 'endee@outlook.com')
+ON DUPLICATE KEY UPDATE ID=ID;
+
+insert into userprofiles values (1, 'lisa', 'lisa@gmail.com')
 ON DUPLICATE KEY UPDATE ID=ID;
