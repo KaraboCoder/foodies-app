b6c6660c73b94ce9bac09d440cdfac35c93fe009
N Nemakhavhani <ndamulelo@bbd.co.za>
2019-04-23 12:42:45 +0200

    Add recipes api controller

---
+++ src/main/java/com/foodies/foodies/Services/Definitions/RecipesService.java
@@ -0,0 +1,47 @@
+package com.foodies.foodies.Services.Definitions;
+
+import com.foodies.foodies.DAO.RecipesRepository;
+import com.foodies.foodies.Models.Recipe;
+import com.foodies.foodies.Services.IRecipesService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RecipesService implements IRecipesService {
+
+    private RecipesRepository _repo;
+
+    @Autowired
+    public RecipesService(RecipesRepository repo) {
+        _repo = repo;
+    }
+
+
+    @Override
+    public Iterable<Recipe> FetchAllRecipes() {
+        return _repo.findAll();
+    }
+
+    @Override
+    public Iterable<Recipe> FetchUsersRecipes(Long UserId) {
+        return null;
+    }
+
+    @Override
+    public Recipe FindRecipeByID(Long ID) {
+        return null;
+    }
+
+    @Override
+    public boolean CreateRecipe(Recipe recipe) {
+        return false;
+    }
+
+    @Override
+    public boolean UpdateRecipe(Long ID, Recipe updated) {
+        return false;
+    }
+
+    @Override
+    public boolean DeleteRecipe(Long userId, Long ID) {
+        return false;
+    }
+}
---
+++ src/main/java/com/foodies/foodies/restApi/RecipesController.java
@@ -0,0 +1,34 @@
+package com.foodies.foodies.restApi;
+
+import com.foodies.foodies.Models.Recipe;
+import com.foodies.foodies.Services.IRecipesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
+public class RecipesController {
+    private IRecipesService _recipeService;
+
+    @Autowired
+    public RecipesController(IRecipesService recipeService) {
+        _recipeService = recipeService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Recipe>> RetriveAllRecipes(){
+
+        List<Recipe> results = new ArrayList<>();
+
+        _recipeService.FetchAllRecipes().forEach(item -> results.add((item)));
+
+        return new ResponseEntity(results, HttpStatus.OK);
+    }
+}
