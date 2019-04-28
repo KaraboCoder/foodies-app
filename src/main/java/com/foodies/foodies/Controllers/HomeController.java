da3cc348d13e2c2b08e49bf30ac4bcdfb4df53e4
N Nemakhavhani <ndamulelo@bbd.co.za>
2019-04-28 13:16:44 +0200

    fix file extensions

---
+++ src/main/java/com/foodies/foodies/Controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.foodies.foodies.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller("")
+public class HomeController {
+
+    @GetMapping("")
+    public  String HomePageView(Model model){
+        model.addAttribute("message", "Thymeleaf is f**n awsome!");
+        model.addAttribute("umessage", "<b>Thymeleaf is here!</b>");
+        return "index";
+    }
+}
